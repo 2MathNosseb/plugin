b6f7a12262a36e49de8bd4dc165653b413c82626
2MathNosseb <m.besson2008@gmail.com>
2024-09-11 18:27:46 +0200

    Update plugin.py

--- plugin.py
+++ plugin.py
@@ -1,4 +1,5 @@
-from server import bot
-@bot.command()
-async def say(ctx):
-  print("bonjour")
+import discord
+from discord.ext import commands
+intents = discord.Intents.default()
+intents.message_content = True
+bot = commands.Bot(command_prefix='!', intents=intents)
